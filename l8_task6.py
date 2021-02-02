18082375854ff3538a0fe41caccb5cdd327e7eee
Sergey <st.apostol@mail.ru>
2021-02-02 19:49:48 +0300

    add export json for task 6

--- l8_task6.py
+++ l8_task6.py
@@ -5,6 +5,7 @@
 # Подсказка: постарайтесь по возможности реализовать в проекте «Склад оргтехники» максимум возможностей, изученных на уроках по ООП.
 
 from abc import ABC, abstractmethod
+import json
 
 
 class Warehouse:
@@ -30,6 +31,39 @@ class Warehouse:
         cls.user_devices[str(device.__class__.__name__)].append(device)
         print(device, 'Передано со склада.')
 
+    @staticmethod
+    def json_export():
+        ware_dev_for_exp = {}
+        ware_list_for_exp = []
+        for key in Warehouse.devices_in_warehouse.keys():
+            ware_list = []
+            for value in Warehouse.devices_in_warehouse[key]: 
+                if value:
+                    ware_list.append(value.get_info())
+            if ware_list:
+                ware_list.append({'Количество':len(ware_list)})
+            ware_dev_for_exp = {key: ware_list} if ware_list else None
+            if ware_dev_for_exp:
+                ware_list_for_exp.append(ware_dev_for_exp)
+        ware_dev_for_exp = {'Устройства на складе': ware_list_for_exp}
+
+        user_dev_for_exp = {}
+        user_list_for_exp = []
+
+        for key in Warehouse.user_devices.keys():
+            ware_list = []
+            for value in Warehouse.user_devices[key]: 
+                if value:
+                    ware_list.append(value.get_info())
+            if ware_list:
+                ware_list.append({'Количество':len(ware_list)})
+            user_dev_for_exp = {key: ware_list} if ware_list else None
+            if user_dev_for_exp:
+                user_list_for_exp.append(user_dev_for_exp)
+        user_dev_for_exp = {'Устройства у пользователей': user_list_for_exp}
+        with open('l8_task6.json', 'w', encoding='utf-8') as out_file:
+            json.dump([ware_dev_for_exp, user_dev_for_exp], out_file, indent=4, ensure_ascii=False)
+
     def __str__(self):
         string_warehouse = '\n'.join(map(str, (sum(self.devices_in_warehouse.values(), []))))
         string_warehouse = 'Устройства на складе:\n' + string_warehouse if string_warehouse else 'На складе нет устройств'
@@ -48,6 +82,10 @@ class Device(ABC):
 
     def __str__(self):
         return 'Устройство {}, номер {}, ответственный {}.'.format(self._brand, self._serial, self._owner)
+
+    def get_info(self):
+        print(self._brand, self._owner, self._serial)
+        return {'Модель': self._brand, 'Ответственный': self._owner, 'Инвентарный номер': self._serial}
 
     def change_owner(self, owner='зав.складом'):
         if Device.valid_owner(owner):
@@ -182,6 +220,7 @@ def main():
     print(Device.how_mush_devices())
     print('******************************')
     print(printer.how_mush_devices())
+    Warehouse.json_export()
 
 if __name__ == '__main__':
     main()
